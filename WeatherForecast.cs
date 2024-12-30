cba47b262f8ee6aa0295ced7fe3b65e7eadf77cc
Srikanth <nivarthi.srikanth@gmail.com>
2024-12-30 10:15:43 +0530

    Add project files.

---
+++ WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace DemoApp3012
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
